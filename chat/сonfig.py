395a00b9124c332c2aaf7a18181d428cdb4aef57
Natalya <57089530+allen-99@users.noreply.github.com>
2022-05-08 13:15:05 +1000

    Delete сonfig.py

--- chat/сonfig.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-class Configuration(object):
-    file_path = os.path.abspath(os.getcwd()) + "/chat.db"
-    SECRET_KEY = "SECRETKEY"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + file_path
